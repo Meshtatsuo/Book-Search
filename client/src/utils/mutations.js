db37853106d5cda98a8886c1244a3758039e7f7e
meshtatsuo <meshtatsuo@gmail.com>
2022-05-20 18:49:39 -0600

    initial work on refactor

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,54 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook(
+    $authors: [String]
+    $desription: String!
+    $bookId: String!
+    $image: String
+    $link: String
+    $title: String!
+  ) {
+    saveBook(
+      authors: $authors
+      description: $description
+      bookId: $bookId
+      image: $image
+      link: $link
+      title: $title
+    ) {
+      Book {
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+    }
+  }
+`;
--- server/server.js
+++ server/server.js
@@ -1,21 +1,47 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const path = require("path");
+
+// import apollo server
+const { ApolloServer } = require("apollo-server-express");
+
+// Import typedefs and resolvers
+const { typeDefs, resolvers } = require("./schemas");
+
+const db = require("./config/connection");
+const routes = require("./routes");
+
+const PORT = process.env.PORT || 3001;
+
+// create Apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  // integrate Apollo with express as middleware
+  server.applyMiddleware({ app });
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      // log GQL API location
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  });
+};
+
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+startApolloServer(typeDefs, resolvers);
